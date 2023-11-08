b14286f566b26d57d6b0025e0f65fb22abb75e89
Emma <elune.lind@gmail.com>
2023-11-08 16:11:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Virtual_Book_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
